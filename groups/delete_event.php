7260bcb160480a15cafcc93778ec6ef83daa7a0b
frostblooded <frostblooded@yahoo.com>
2021-01-26 13:05:56 +0200

    Fix event delete permissions

--- groups/delete_event.php
+++ groups/delete_event.php
@@ -1,19 +1,19 @@
 <?php 
     function check_for_errors() {
         $user = get_user();
-        $post_id = $_POST['post_id'];
         $group_id = $_POST['group_id'];
+        $event_id = $_POST['event_id'];
 
         $conn = new_db_connection();
-        $stmt = $conn->prepare("SELECT user_id FROM posts WHERE id = ?;");
-        $stmt->execute([$post_id]);
-        $post = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt = $conn->prepare("SELECT user_id FROM events WHERE id = ?;");
+        $stmt->execute([$event_id]);
+        $event = $stmt->fetch(PDO::FETCH_ASSOC);
 
         $stmt = $conn->prepare("SELECT admin_id FROM groups WHERE id = ?;");
         $stmt->execute([$group_id]);
         $group = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if($user['id'] != $post['user_id'] && $user['id'] != $group['admin_id']) {
+        if($user['id'] != $event['user_id'] && $user['id'] != $group['admin_id']) {
             set_errors(Array("Не можеш да триеш събития в тази група!"));
             return true;
         }
